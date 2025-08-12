17d9971a8bc24740a36912130c94ca7259edd07f
Annee Barrett <annee.barrett@stardog.com>
2025-08-12 16:28:24 -0400

    Sort scss module imports last (#2)
    * order scss module imports
    
    * fix comment

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ module.exports = function (styleApi) {
     unicode,
   } = styleApi;
 
+  function endsWith(...suffixes) {
+    return (text) => {
+      return suffixes.some((suffix) => text.endsWith(suffix));
+    };
+  }
+
   return [
     // import "foo";
     { match: and(hasNoMember, isAbsoluteModule) },
@@ -24,7 +30,11 @@ module.exports = function (styleApi) {
 
     // import … from "foo";
     {
-      match: and(isAbsoluteModule, not(moduleName(startsWith("bones"))), not(moduleName(startsWith("src")))),
+      match: and(
+        isAbsoluteModule,
+        not(moduleName(startsWith('bones'))),
+        not(moduleName(startsWith('src')))
+      ),
       sort: moduleName(naturally),
       sortNamedMembers: alias(unicode),
     },
@@ -32,15 +42,19 @@ module.exports = function (styleApi) {
 
     // import … from "bones/foo";
     {
-      match: and(isAbsoluteModule, moduleName(startsWith("bones"))),
+      match: and(isAbsoluteModule, moduleName(startsWith('bones'))),
       sort: moduleName(naturally),
       sortNamedMembers: alias(unicode),
     },
     { separator: true },
-    
+
     // import … from "src/foo";
     {
-      match: and(isAbsoluteModule, moduleName(startsWith("src"))),
+      match: and(
+        isAbsoluteModule,
+        moduleName(startsWith('src')),
+        not(moduleName(endsWith('.module.scss')))
+      ),
       sort: moduleName(naturally),
       sortNamedMembers: alias(unicode),
     },
@@ -51,6 +65,18 @@ module.exports = function (styleApi) {
     {
       match: isRelativeModule,
       sort: [dotSegmentCount, moduleName(naturally)],
+      sortNamedMembers: alias(unicode),
+    },
+    { separator: true },
+
+    // import … from "src/foo.module.scss";
+    {
+      match: and(
+        isAbsoluteModule,
+        moduleName(startsWith('src')),
+        moduleName(endsWith('.module.scss'))
+      ),
+      sort: moduleName(naturally),
       sortNamedMembers: alias(unicode),
     },
     { separator: true },
